a77b0239245d43847be3de0529fcbded7faba825
MaudeChapleau <MaudeChapleau@LXXX-MXL9271MZW>
2019-09-18 15:25:20 -0400

    Initial non fonctionnel

---
+++ app.js
@@ -0,0 +1,11 @@
+const config = require('config');
+const express = require('express');
+const lib = require('./lib');
+
+const app = express();
+
+//Configuration de la base de données
+//require('./lib/database');
+
+//Configuration des modèles de la base de données
+//require('./models');
