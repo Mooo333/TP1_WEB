4902ca25bbd8e89aef7973becbb125c130082a0b
MaudeChapleau <MaudeChapleau@LXXX-MXL9271MZW>
2019-09-20 09:56:19 -0400

    Empecher delete/path/put

--- routes/livres.js
+++ routes/livres.js
@@ -11,4 +11,19 @@ router.post("/livres/{uuidLivre}", async(req, res, next) => {
 router.get("/{uuidLivre}/inventaire", async(req, res, next) => {
     
 });
+
+
+
+router.delete('/', (req, res, next) => {
+    next(new createError.MethodNotAllowed());
+});
+
+router.patch('/', (req, res, next) => {
+    next(new createError.MethodNotAllowed());
+});
+
+router.put('/', (req, res, next) => {
+    next(new createError.MethodNotAllowed());
+});
+
 module.exports = router;
--- routes/succursales.js
+++ routes/succursales.js
@@ -60,4 +60,17 @@ router.post("/{uuidSucursale}", async(req, res, next) => {
     
 });
 
+
+router.delete('/', (req, res, next) => {
+    next(new createError.MethodNotAllowed());
+});
+
+router.patch('/', (req, res, next) => {
+    next(new createError.MethodNotAllowed());
+});
+
+router.put('/', (req, res, next) => {
+    next(new createError.MethodNotAllowed());
+});
+
 module.exports = router;
