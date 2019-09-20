c9607f2308e2bc0ab58936c032220ca97fa3ef4f
MaudeChapleau <MaudeChapleau@LXXX-MXL9271MZW>
2019-09-20 10:26:29 -0400

    Routes Maude

--- routes/livres.js
+++ routes/livres.js
@@ -15,20 +15,28 @@ router.get("/{uuidLivre}/inventaire", async(req, res, next) => {
 // ******* Routes de Maude *******
 // URL:        /categories
 // Réponse:    Collection sans meta-data
-
+router.get("/categories", async(req, res, next) => {
+    
+});
 // URL:        /livres/{uuidLivre}
 // Parametres: expand (collection d'inventaire) & fields (select. attrib. spécif.)
 // Réponse:    Objet
-
+router.get("/livres/{uuidLivre}", async(req, res, next) => {
+    
+});
 // URL:        /livres/{uuidLivre}
 // Parametres: _body permet de retourner la représ. de l'objet dans la réponse
 // Réponse:    Objet
-
+router.patch("/livres/{uuidLivre}", async(req, res, next) => {
+    
+});
 // URL:        /livres/{uuidLivre}/commentaires
 // Parametres: _body permet de retourner la représ. de l'objet dans la réponse
 // Réponse:    Objet
 // Ajouter un header
-
+router.post("/livres/{uuidLivre}/commentaires", async(req, res, next) => {
+    
+});
 
 
 // *******************************
