19d0fdaae66f3c929154bda36de73a2775b2e92d
MaudeChapleau <MaudeChapleau@LXXX-MXL9271MZW>
2019-09-20 10:22:51 -0400

    Route Maude (patch enlevé)

--- routes/livres.js
+++ routes/livres.js
@@ -12,13 +12,28 @@ router.get("/{uuidLivre}/inventaire", async(req, res, next) => {
     
 });
 
+// ******* Routes de Maude *******
+// URL:        /categories
+// Réponse:    Collection sans meta-data
 
+// URL:        /livres/{uuidLivre}
+// Parametres: expand (collection d'inventaire) & fields (select. attrib. spécif.)
+// Réponse:    Objet
+
+// URL:        /livres/{uuidLivre}
+// Parametres: _body permet de retourner la représ. de l'objet dans la réponse
+// Réponse:    Objet
+
+// URL:        /livres/{uuidLivre}/commentaires
+// Parametres: _body permet de retourner la représ. de l'objet dans la réponse
+// Réponse:    Objet
+// Ajouter un header
+
+
+
+// *******************************
 
 router.delete('/', (req, res, next) => {
-    next(new createError.MethodNotAllowed());
-});
-
-router.patch('/', (req, res, next) => {
     next(new createError.MethodNotAllowed());
 });
 
