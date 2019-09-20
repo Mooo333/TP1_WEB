95552dbf14e06b6532f2a785b310d9c753af6c7c
Vincent Leygues <1655932@cstj.qc.ca>
2019-09-20 08:41:40 -0400

    modifs Vincent 1

--- routes/livres.js
+++ routes/livres.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const router = express.Router();
-const Succursale = mongoose.model('Livres');
+const Succursale = mongoose.model('livre');
 
 router.post("/livres/{uuidLivre}", async(req, res, next) => {
 
